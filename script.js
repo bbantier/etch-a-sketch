f61d666c300a74ff641cebcb20e07f1bfe542e98
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-17 12:03:44 +0100

    complete base functionality in script.js

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const grid = document.querySelector("#grid");
 
 function drawGrid() {
   for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    grid.appendChild(div);
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    grid.appendChild(gridItem);
   }
+  const gridItem = document.querySelectorAll(".grid-item");
+  gridItem.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      colorOnHover(item);
+    });
+  });
+}
+
+function colorOnHover(element) {
+  element.style.backgroundColor = "gray";
 }
 
 document.addEventListener("DOMContentLoaded", () => drawGrid());
