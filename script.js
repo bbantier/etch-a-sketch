78a9956a42b51f441633069e1d55d051f025813a
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-17 11:11:57 +0100

    fix loop iterations and CSS flex

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector("#grid");
 
 function drawGrid() {
-  for (let i = 1; i < 256; i++) {
+  for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-item");
     grid.appendChild(div);
