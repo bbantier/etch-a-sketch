db5fddbab7c15c3576e4d463fb9ba2e5c20506fb
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-17 11:01:50 +0100

    add styles.css and write drawGrid function

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector("#grid");
+
+function drawGrid() {
+  for (let i = 1; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    grid.appendChild(div);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => drawGrid());
