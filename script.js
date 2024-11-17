785cbbce2eacb4edb39b430ccec60d25da5b5ef6
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-17 12:30:16 +0100

    allow user to change grid size with a button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector("#grid");
+const gridButton = document.querySelector("#grid-button");
 
-function drawGrid() {
-  for (let i = 1; i <= 256; i++) {
+function drawGrid(gridSize = 16) {
+  for (let i = 1; i <= gridSize ** 2; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     grid.appendChild(gridItem);
@@ -18,4 +19,12 @@ function colorOnHover(element) {
   element.style.backgroundColor = "gray";
 }
 
+function changeGridSize() {
+  let gridSize = prompt("What size do you want the grid to be? (max. 100)");
+  if (gridSize > 100) {gridSize = 100};
+  grid.innerHTML = "";
+  drawGrid(gridSize);
+}
+
 document.addEventListener("DOMContentLoaded", () => drawGrid());
+gridButton.addEventListener("click", () => changeGridSize());
